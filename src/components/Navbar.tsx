24772489666314dac82507a42ca1ac313f8b3f6b
Yogesh <chauhanyogesh950@gmail.com>
2024-05-10 23:48:01 +0530

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = (props: Props) => {
     <>
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
         <div  className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-            <Link  className="text-xl font-bold mb-4 md:mb-0" href={"#"}>Mystery Message</Link>
+            <Link  className="text-xl font-bold mb-4 md:mb-0" href={"/"}>Mystery Message</Link>
 
             {
                 session? (
